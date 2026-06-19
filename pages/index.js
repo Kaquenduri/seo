fe9fa251b02bf0cd8e215e9e1ea2a7b2bb49b24c
Kaquenduri <marchunque7880@gmail.com>
2026-06-18 19:57:18 -0500

    feat: initial commit 1

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta name="google-site-verification" content="GxqG_0MMpVfL7wrXycetQd-xQ3id_IHeodluiyTiZ-Q" />
+        <meta name="google-site-verification" content="N6haaIn0wuDYyJt1kJgOXcMr8DrbBpa-Fq_6VqaPyoI" />
         <title>Agencia SEO Digital | Incrementa tu Tráfico Web</title>
         <meta name="description" content="Especialistas en optimización SEO y rendimiento web con Next.js. Lleva tu negocio a los primeros lugares de Google." />
         <meta name="keywords" content="Agencia SEO, optimización web, Next.js, tráfico web" />
